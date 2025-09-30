a33b9d47c4cb1f5c0cc422e6561af731bbf59658
ALEXANDRE PAVANELLO E SILVA <apavanello@live.com>
2025-09-30 19:03:27 -0300

    remove default admin groups and users; update DisplayItem admin list display to include order and mute

--- videoCycler/admin.py
+++ videoCycler/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
 from videoCycler.models import DisplayItem
 from unfold.admin import ModelAdmin
 
-# Register your models here.
+# remover os grupos e usuários padrão do admin
+for model in (Group, User):
+    if model in admin.site._registry:
+        admin.site.unregister(model)
+
 
 @admin.register(DisplayItem)
 class CustomAdminClass(ModelAdmin):
-    list_display = ('title', 'media_type', 'duration_in_seconds')
+    list_display = ('title', 'media_type', 'duration_in_seconds', 'order', 'mute')
     list_filter = ('media_type',)
     search_fields = ('title',)
-
-
